884d894f98737c8d2c7b06b3455b4fd39ffe1177
marcelo-rufino <marcelorufinomaria@hormail.com>
2020-11-05 12:20:34 -0300

    projeto php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Influência dos Games</title>
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+
+    <body>
+    <!--Início do menu-->
+    <?php
+    include('menu.html');
+    ?>
+    <!--Fim do menu-->
+
+    <p class="texto_inicial">Olá Gamers!<p/>
+
+    <p class="subtitulos">Se você gosta de games está no lugar certo.</>
+
+    <h3>Só aqui você encontra a maior variedade referente a games e periféricos.</h3>
+
+    <br><br><br><br><br>
+    <br><br><br><br><br>
+    <br><br><br><br><br>
+                 
+    <footer id="rodape">
+        <p id="formas_pagamento"><b>Formas de pagamento</b></p>
+        <img src="./imagens/formas de pagamento.png" width="400px">
+        <p>&copy; Influência dos Games</p>
+    </footer>
+
+    </body>
+</html>
